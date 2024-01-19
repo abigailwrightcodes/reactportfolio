a0acb34244f7743a2a0a72370e4aa3ea13a3727e
abigailwright89 <102739142+abigailwright89@users.noreply.github.com>
2024-01-19 20:45:29 +0000

    image test

--- src/components/3.PortfolioPage/projectData.js
+++ src/components/3.PortfolioPage/projectData.js
@@ -2,28 +2,28 @@
 
 const ProjectData = [
     [
-      { title: 'Password Generator', description: 'Description for Project 1 - Card 1', imageUrl: 'src/components/images/portfolio-coming-soon.png' , moreInfo: 'Link or Action'},
+      { title: 'Password Generator', description: 'Description for Project 1 - Card 1', imageUrl: './src/components/images/portfolio-coming-soon.png' , moreInfo: 'Link or Action'},
      
     ],
     [
-      { title: 'Project 2 - Card 1', description: 'Description for Project 2 - Card 1', imageUrl: 'src/components/images/portfolio-coming-soon.png' , moreInfo: 'Link or Action' },
+      { title: 'Project 2 - Card 1', description: 'Description for Project 2 - Card 1', imageUrl: './src/components/images/portfolio-coming-soon.png' , moreInfo: 'Link or Action' },
 
     ],
     [
-        { title: 'Project 3 - Card 1', description: 'Description for Project 3 - Card 1', imageUrl: 'src/components/images/portfolio-coming-soon.png' , moreInfo: 'Link or Action' },
+        { title: 'Project 3 - Card 1', description: 'Description for Project 3 - Card 1', imageUrl: './src/components/images/portfolio-coming-soon.png' , moreInfo: 'Link or Action' },
       
       ],
 
       [
-        { title: 'Project 4', description: 'Description for Project 1 - Card 1', imageUrl: '' , moreInfo: 'Link or Action'},
+        { title: 'Project 4', description: 'Description for Project 1 - Card 1', imageUrl: './src/components/images/portfolio-coming-soon.png' , moreInfo: 'Link or Action'},
        
       ],
       [
-        { title: 'Project 5 - Card 1', description: 'Description for Project 2 - Card 1', imageUrl: '' , moreInfo: 'Link or Action' },
+        { title: 'Project 5 - Card 1', description: 'Description for Project 2 - Card 1', imageUrl: './src/components/images/portfolio-coming-soon.png' , moreInfo: 'Link or Action' },
   
       ],
       [
-          { title: 'Project 6 - Card 1', description: 'Description for Project 3 - Card 1', imageUrl: '' , moreInfo: 'Link or Action' },
+          { title: 'Project 6 - Card 1', description: 'Description for Project 3 - Card 1', imageUrl: './src/components/images/portfolio-coming-soon.png' , moreInfo: 'Link or Action' },
         
         ],
       
